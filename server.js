e6be4b719a5491670f180aca74f47fd7ac096c26
ayaabaazumahmichaela-glitch <ayaabaazumahmichaela@gmail.com>
2026-08-29 11:49:03 +0000

    Log Paystack secret key loading status

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const SECRET_KEY = process.env.PAYSTACK_SECRET_KEY;
+console.log("PAYSTACK KEY LOADED:", !!process.env.PAYSTACK_SECRET_KEY);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
